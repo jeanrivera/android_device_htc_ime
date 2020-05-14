1799c76cffdce1be01c959d2e83b67b984bca43e
jeanrivera <jeanrivera@gmail.com>
2020-05-14 19:06:31 -0400

    Update omni_ime.mk

--- omni_ime.mk
+++ omni_ime.mk
@@ -20,7 +20,7 @@ PRODUCT_RELEASE_NAME := ime
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm845
